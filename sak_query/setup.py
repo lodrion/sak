e18301de5679af9030718258b4f5b277bedd81b7
leonid <lodrion@gmail.com>
2020-02-02 13:45:07 -0500

    Fix package name, add import

--- sak_query/setup.py
+++ sak_query/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='sak-http',
+    name='sak-query',
     version='0.0.1',
     packages=find_packages(),
     install_requires=["requests>=2.22.0"],
